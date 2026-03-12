0a6b98878351639a99dafa85fb6ad9a154719d4f
agolitorenerose5-dev <agolitorenerose5@gmail.com>
2026-03-12 13:17:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-return "Welcome to my Flask API!"
+    return "App is running!"
 
-@app.route('/student')
-def get_student():
-return jsonify({
-"name": "Your Name",
-"grade": 10,
-"section": "Zechariah"
-})
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
